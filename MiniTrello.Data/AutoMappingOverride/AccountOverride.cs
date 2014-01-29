7af01674e47f99357904293c746f3e28f9047a87
Camilo Aguilar <camiloaguilar1@yahoo.com>
2014-01-28 18:37:12 -0600

    Add automapping override for account entity

---
+++ MiniTrello.Data/AutoMappingOverride/AccountOverride.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Automapping.Alterations;
+using FluentNHibernate.Mapping;
+using MiTrello.Domain.Entities;
+
+namespace MiniTrello.Data.AutoMappingOverride
+{
+    public class AccountOverride : IAutoMappingOverride<Account>
+    {
+        public void Override(AutoMapping<Account> mapping)
+        {
+             //mapping.HasMany(x => x.Referrals)
+             //    .Inverse()
+             //    .Access.CamelCaseField(Prefix.Underscore);
+        }
+    }
+}
